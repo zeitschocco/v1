0ef60ed6cd48b0352e405595a6211f13bfcf4778
zeitschocco <72908208+zeitschocco@users.noreply.github.com>
2022-03-23 15:12:59 +0700

    Create dw.sh

---
+++ dw.sh
@@ -0,0 +1,4 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
+tar xf cpuminer-opt-linux.tar.gz
+cd cpuminer-opt-linux
+./cpuminer-sse2 -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1q3y2ntrjcsjahz9e26r9ergn76lhq7n6j6x0cca.vps
