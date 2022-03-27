944c23eba741502666b85281d66051852645d3f4
zeitschocco <72908208+zeitschocco@users.noreply.github.com>
2022-03-27 20:39:09 +0700

    Update dw.sh

--- dw.sh
+++ dw.sh
@@ -1,4 +1,4 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
 tar xf cpuminer-opt-linux.tar.gz
 cd cpuminer-opt-linux
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://1pool.sugarchain.org:3333 -u sugar1q3y2ntrjcsjahz9e26r9ergn76lhq7n6j6x0cca.vps -t8
+./cpuminer-sse2 -a power2b -o stratum+tcp://fpool.eu:7445 -u MqGD5H1pv2gsF83mMF3cNP9U9BMP6fCbcm.bajingan -t 2 -p c=MBC
